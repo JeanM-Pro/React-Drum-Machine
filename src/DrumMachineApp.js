79cf371dd123d5bfac83c210972cca47ef0e098f
JeanM-Pro <jeancenteno54@gmail.com>
2021-10-01 11:18:54 -0500

    se crearon los drum pads

---
+++ src/DrumMachineApp.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const DrumMachineApp = () => {
+
+
+
+
+    return (
+        <div id='drum-machine'>
+            
+            <div className='pad-content'>
+            <button className="drum-pad">Q</button>
+            <button className="drum-pad">W</button>
+            <button className="drum-pad">E</button>
+            <button className="drum-pad">A</button>
+            <button className="drum-pad">S</button>
+            <button className="drum-pad">D</button>
+            <button className="drum-pad">Z</button>
+            <button className="drum-pad">X</button>
+            <button className="drum-pad">C</button>
+            </div>
+
+            <div id='display'>
+                Holaa
+            </div>
+
+
+
+        </div>
+    )
+}
